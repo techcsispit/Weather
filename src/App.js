bf23c2b11283adbe92644759904a776e6a8c27b6
SoulDev07 <mayankravariya@gmail.com>
2023-10-24 14:46:53 +0530

    Update Project Structure

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React,{useState}  from "react";
+import React, { useState } from "react";
 import Search from "./components/Search"
 import Tempdata from "./components/Tempdata";
-import logo from "./weather-logo.png";
 import Footer from "./components/Footer";
+import logo from "./images/weather-logo.png";
 
 function App() {
-  const [city, setCity]=useState("");
+  const [city, setCity] = useState("");
 
-  function setCityInput(cityinput){
+  function setCityInput(cityinput) {
     setCity(cityinput);
-   
   }
   return (
     <>
       <div className="out-container">
-        
+
         <div className="main-container" id="main-container">
-        <div className="logo-container">
-        <img src={logo} id="logo"></img>
-        <h2>WeatherWand</h2>
-        
-        </div>
+          <div className="logo-container">
+            <img src={logo} id="logo" alt="Icon"></img>
+            <h2>WeatherWand</h2>
+          </div>
           <Search sendCity={setCityInput}></Search>
           <Tempdata city={city}></Tempdata>
           <div>
@@ -29,7 +27,6 @@ function App() {
 
         <Footer></Footer>
       </div>
-
     </>
   );
 }
--- src/bgchangescript.js
+++ src/bgchangescript.js
@@ -1,30 +1,24 @@
-import morning from "./morning.jpg";
-import afternoon from "./afternoon2.jpg";
-import evening from "./evening3.jpg";
-import night from "./night1.webp";
+import morning from "./images/morning.jpg";
+import afternoon from "./images/afternoon2.jpg";
+import evening from "./images/evening.jpg";
+import night from "./images/night.jpg";
 
 let hours = new Date().getHours();
-setTimeout(()=>{Bgchange(hours)}, 10)
+setTimeout(() => { Bgchange(hours); }, 10);
 
 
 function Bgchange(hours) {
     setTimeout(() => {
-    const bgimage = document.getElementById("main-container")
+        const bgimage = document.getElementById("main-container");
 
-    if (hours >= 5 && hours < 10) {
-        bgimage.style.backgroundImage = `url(${morning})`;
-    }
-    else if (hours >= 10 && hours < 16) {
-        bgimage.style.backgroundImage = `url(${afternoon})`;
-
-    }
-    else if (hours >= 16 && hours < 20) {
-        // bgimage.style.backgroundImage = "url('../public/evening3.jpg')";
-        bgimage.style.backgroundImage = `url(${evening})`;
-    }
-    else if(hours<5 || hours>=20){
-        bgimage.style.backgroundImage = `url(${night})`;
-    }
-},2);
+        if (hours >= 5 && hours < 10)
+            bgimage.style.backgroundImage = `url(${morning})`;
+        else if (hours >= 10 && hours < 16)
+            bgimage.style.backgroundImage = `url(${afternoon})`;
+        else if (hours >= 16 && hours < 20)
+            bgimage.style.backgroundImage = `url(${evening})`;
+        else
+            bgimage.style.backgroundImage = `url(${night})`;
+    }, 2);
 }
 export default Bgchange;
