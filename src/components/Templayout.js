7fe5d50d5c1d963095fa88536b28d914e5134355
SoulDev07 <mayankravariya@gmail.com>
2023-10-24 16:11:54 +0530

    Refactor Templayout.js

--- src/components/Templayout.js
+++ src/components/Templayout.js
@@ -1,274 +1,112 @@
 // icon and data
 import React, { useState } from "react";
 import Bgchange from "../bgchangescript.js";
+
+const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const months = ["January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"];
+const iconURL = "https://openweathermap.org/img/wn/";
+
 function Templayout(props) {
-    const temperature = Math.round((props.temp1.temp) - 273);
+    // Get weather details
     const weather = props.weather1.main;
     const weatherdesc = props.weather1.description;
-    const city = props.city1;
-    const country = props.country1;
-    const mintemp = Math.round((props.temp1.temp_min) - 273);
-    const maxtemp = Math.round((props.temp1.temp_max) - 273);
-    const timestamp = props.timestamp1;
-    const timezone = props.timezone1;
+    const weathericon = props.weather1.icon;
     const humidity = props.temp1.humidity;
     const pressure = props.temp1.pressure;
     const windspeed = props.wind1.speed;
     const winddirection = props.wind1.deg;
+    // Get temperature details
+    const temperature = Math.round((props.temp1.temp) - 273);
+    const mintemp = Math.round((props.temp1.temp_min) - 273);
+    const maxtemp = Math.round((props.temp1.temp_max) - 273);
+    // Get details of location of user
+    const city = props.city1;
+    const country = props.country1;
+    const timestamp = props.timestamp1;
+    const timezone = props.timezone1;
 
     let date = new Date();
+
+    // Periodically update date every 6 seconds
     setInterval(() => {
         date = new Date();
+        const utc = date.getTime() + (date.getTimezoneOffset() * 60000);
+        date = new Date(utc + (1000 * timezone));
     }, 6000);
 
-   
-    const utc = date.getTime() + (date.getTimezoneOffset() * 60000);
-    date = new Date(utc + (1000 * timezone));
-
+    // Get timing details
     let hours = date.getHours();
     const minutes = date.getMinutes();
     const seconds = date.getSeconds();
-    const day = date.getDay();
     const dateofmonth = date.getDate();
-    const month = date.getMonth();
-    let monthtext = "";
-    let weekday = "";
-
-    Bgchange(hours);
-    
-    switch (day) {
-        case 0: {
-            weekday = "Sunday";
-            break;
-        }
-        case 1: {
-            weekday = "Monday";
-            break;
-        }
-        case 2: {
-            weekday = "Tuesday";
-            break;
-        }
-        case 3: {
-            weekday = "Wednesday";
-            break;
-        }
-        case 4: {
-            weekday = "Thursday";
-            break;
-        }
-        case 5: {
-            weekday = "Friday";
-            break;
-        }
-        case 6: {
-            weekday = "Saturday";
-            break;
-        }
-
-    }
-
-
-
-    switch (month) {
-        case 0: {
-            monthtext = "January";
-            break;
-        }
-        case 1: {
-            monthtext = "February";
-            break;
-        }
-        case 2: {
-            monthtext = "March";
-            break;
-        }
-        case 3: {
-            monthtext = "April";
-            break;
-        }
-        case 4: {
-            monthtext = "May";
-            break;
-        }
-        case 5: {
-            monthtext = "June";
-            break;
-        }
-        case 6: {
-            monthtext = "July";
-            break;
-        }
-        case 7: {
-            monthtext = "August";
-            break;
-        }
-        case 8: {
-            monthtext = "September";
-            break;
-        }
-        case 9: {
-            monthtext = "October";
-            break;
-        }
-        case 10: {
-            monthtext = "November";
-            break;
-        }
-        case 11: {
-            monthtext = "December";
-            break;
-        }
-
-    }
-
+    let month = months[date.getMonth()];
+    let weekday = weekdays[date.getDay()];
     let year = date.getFullYear();
 
+    // Periodically change background based on current time
+    Bgchange(hours);
 
-
+    // Returns Minimum and Maximum temperature of the day
     function minmax() {
-        if (mintemp === maxtemp) {
-            return;
-        }
-        else {
+        if (mintemp !== maxtemp)
             return (
                 <div className="minmaxtemp">
                     {mintemp}°C - {maxtemp}°C
                 </div>
-            )
-        }
+            );
     }
+
+    // Function to set weather icon
     function setIcon() {
-        if (hours >= 6 || hours < 19) {
-            return setIconD();
-        }
-        else {
-            return setIconN();
-        }
-    }
-    function setIconN() {
-        switch (weather) {
-            case "Thunderstorm": return (<img src="https://openweathermap.org/img/wn/11n@2x.png"></img>);
-            case "Drizle": return (<img src="https://openweathermap.org/img/wn/09n@2x.png"></img>);
-            case "Rain": {
-                switch (weatherdesc) {
-                    case "freezing rain": return (<img src="https://openweathermap.org/img/wn/13n@2x.png"></img>);
-                    case "light intensity shower rain": return (<img src="https://openweathermap.org/img/wn/09n@2x.png"></img>);
-                    case "shower rain": return (<img src="https://openweathermap.org/img/wn/09n@2x.png"></img>);
-                    case "heavy intensity shower rain": return (<img src="https://openweathermap.org/img/wn/09n@2x.png"></img>);
-                    case "ragged shower rain": return (<img src="https://openweathermap.org/img/wn/09n@2x.png"></img>);
-                    default: return (<img src="https://openweathermap.org/img/wn/10n@2x.png"></img>);
-                }
-
-            };
-            case "Mist": return (<img src="https://openweathermap.org/img/wn/50n@2x.png"></img>);
-            case "Smoke": return (<img src="https://openweathermap.org/img/wn/50n@2x.png"></img>);
-            case "Haze": return (<img src="https://openweathermap.org/img/wn/50n@2x.png"></img>);
-            case "Dust": return (<img src="https://openweathermap.org/img/wn/50n@2x.png"></img>);
-            case "Fog": return (<img src="https://openweathermap.org/img/wn/50n@2x.png"></img>);
-            case "Sand": return (<img src="https://openweathermap.org/img/wn/50n@2x.png"></img>);
-            case "Ash": return (<img src="https://openweathermap.org/img/wn/50n@2x.png"></img>);
-            case "Squall": return (<img src="https://openweathermap.org/img/wn/50n@2x.png"></img>);
-            case "Tornado": return (<img src="https://openweathermap.org/img/wn/50d@2x.png"></img>);
-            case "Clear": return (<img src="https://openweathermap.org/img/wn/01d@2x.png"></img>);
-            case "Clouds": {
-                switch (weatherdesc) {
-                    case "few clouds": return (<img src="https://openweathermap.org/img/wn/02n@2x.png"></img>);
-                    case "scattered clouds": return (<img src="https://openweathermap.org/img/wn/03n@2x.png"></img>);
-                    default: return (<img src="https://openweathermap.org/img/wn/04n@2x.png"></img>);
-                }
-            }
-
-        }
-
-    }
-    function setIconD() {
-        // const dayornight="d";
-
-        switch (weather) {
-            case "Thunderstorm": return (<img src="https://openweathermap.org/img/wn/11d@2x.png"></img>);
-            case "Drizle": return (<img src="https://openweathermap.org/img/wn/09d@2x.png"></img>);
-            case "Rain": {
-                switch (weatherdesc) {
-                    case "freezing rain": return (<img src="https://openweathermap.org/img/wn/13d@2x.png"></img>);
-                    case "light intensity shower rain": return (<img src="https://openweathermap.org/img/wn/09d@2x.png"></img>);
-                    case "shower rain": return (<img src="https://openweathermap.org/img/wn/09d@2x.png"></img>);
-                    case "heavy intensity shower rain": return (<img src="https://openweathermap.org/img/wn/09d@2x.png"></img>);
-                    case "ragged shower rain": return (<img src="https://openweathermap.org/img/wn/09d@2x.png"></img>);
-                    default: return (<img src="https://openweathermap.org/img/wn/10d@2x.png"></img>);
-                }
-
-            };
-            case "Mist": return (<img src="https://openweathermap.org/img/wn/50d@2x.png"></img>);
-            case "Smoke": return (<img src="https://openweathermap.org/img/wn/50d@2x.png"></img>);
-            case "Haze": return (<img src="https://openweathermap.org/img/wn/50d@2x.png"></img>);
-            case "Dust": return (<img src="https://openweathermap.org/img/wn/50d@2x.png"></img>);
-            case "Fog": return (<img src="https://openweathermap.org/img/wn/50d@2x.png"></img>);
-            case "Sand": return (<img src="https://openweathermap.org/img/wn/50d@2x.png"></img>);
-            case "Ash": return (<img src="https://openweathermap.org/img/wn/50d@2x.png"></img>);
-            case "Squall": return (<img src="https://openweathermap.org/img/wn/50d@2x.png"></img>);
-            case "Tornado": return (<img src="https://openweathermap.org/img/wn/50d@2x.png"></img>);
-            case "Clear": return (<img src="https://openweathermap.org/img/wn/01d@2x.png"></img>);
-            case "Clouds": {
-                switch (weatherdesc) {
-                    case "few clouds": return (<img src="https://openweathermap.org/img/wn/02d@2x.png"></img>);
-                    case "scattered clouds": return (<img src="https://openweathermap.org/img/wn/03d@2x.png"></img>);
-                    default: return (<img src="https://openweathermap.org/img/wn/04d@2x.png"></img>);
-                }
-            }
-
-        }
-
-    }
-    let ampm="";
-    function setHours(){
-        if(hours>12 && hours<=23){
-            hours=hours-12;
-            ampm="PM";
-        }
-        else if(hours===0){
-            hours=12;
-            ampm="AM";
-        }
-        else{
-            ampm="AM";
-        }
+        return (<img src={`${iconURL + weathericon}@2x.png`} alt={weatherdesc}></img>);
     }
 
-    function setMinutes(){
-        if(minutes<10){
-            return(<span>0{minutes}</span>);
+    // Set hours
+    let ampm = "";
+    function setHours() {
+        if (hours > 12 && hours <= 23) {
+            hours = hours - 12;
+            ampm = "PM";
         }
-        else{
-            return (<span>{minutes}</span>);
+        else if (hours === 0) {
+            hours = 12;
+            ampm = "AM";
         }
-
+        else
+            ampm = "AM";
+        return hours;
     }
-    setHours();
-    
+
+    // Set minutes
+    function setMinutes() {
+        if (minutes < 10)
+            return (<span>0{minutes}</span>);
+        return (<span>{minutes}</span>);
+    }
 
     return (
         <div>
             <div className="outer-data">
+
                 <div className="icon-desc">
                     <div className="data-icon">
                         {setIcon()}
                     </div>
                     <div className="data-weather">
-                        <h2> {weather}</h2>
+                        <h2>{weather}</h2>
                         <p>{weatherdesc}</p>
                     </div>
                 </div>
+
                 <div className="data">
                     <div className="humidity-pressure">
-
                         <div className="data-humidity">
                             <i className="fa fa-thermometer-half"></i>
                             <p>Humidity: {humidity}%</p></div>
                         <div className="data-pressure">
                             <i className="fa-solid fa-gauge-high"></i>
                             <p>Pressure: {pressure} hPa</p></div>
-
                     </div>
 
                     <div className="data-temp">
@@ -282,14 +120,10 @@ function Templayout(props) {
                             <div className="data-minmax">
                                 {minmax()}
                             </div>
-
-
                         </div>
-
-
                     </div>
-                    <div className="wind-data">
 
+                    <div className="wind-data">
                         <div className="data-speed">
                             <i className="fa-solid fa-wind"></i>
                             <p>Wind speed: {windspeed} m/s</p>
@@ -297,18 +131,15 @@ function Templayout(props) {
                         <div className="data-direction">
                             <i className="fa-solid fa-compass"></i>
                             <p>Wind direction: {winddirection} deg</p></div>
-
                     </div>
                 </div>
-
             </div>
-            <div className="date-time">
 
-                <div className="date"> {weekday}, {dateofmonth} {monthtext} {year} </div>
-                <div className="time">{hours}:{setMinutes()} {ampm}</div>
+            <div className="date-time">
+                <div className="date"> {weekday}, {dateofmonth} {month} {year} </div>
+                <div className="time">{setHours()}:{setMinutes()} {ampm}</div>
             </div>
         </div>
-        
     );
 }
 
