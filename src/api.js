78452f84f7de3b446714adfd7838f90ffbd5a506
Varun J <vnjhaveri@gmail.com>
2023-10-24 12:12:29 +0530

    Light/Dark mode, transitions, .gitignore and some dependency fixes

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+    method: "GET",
+    url: 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities',
+    headers: {
+      'X-RapidAPI-Key': '507fb70b96mshd51bfbd56274932p194304jsn78dd15975bdc',
+      'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    },
+  };
+  export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
--- src/bgchangescript.js
+++ src/bgchangescript.js
@@ -1,11 +1,10 @@
 import morning from "./morning.jpg";
 import afternoon from "./afternoon2.jpg";
-import evening from "./evening3.jpg";
+import evening from "./evening3.gif";
 import night from "./night1.webp";
 
 let hours = new Date().getHours();
 setTimeout(()=>{Bgchange(hours)}, 10)
-
 
 function Bgchange(hours) {
     setTimeout(() => {
@@ -16,15 +15,16 @@ function Bgchange(hours) {
     }
     else if (hours >= 10 && hours < 16) {
         bgimage.style.backgroundImage = `url(${afternoon})`;
-
     }
     else if (hours >= 16 && hours < 20) {
-        // bgimage.style.backgroundImage = "url('../public/evening3.jpg')";
         bgimage.style.backgroundImage = `url(${evening})`;
     }
     else if(hours<5 || hours>=20){
         bgimage.style.backgroundImage = `url(${night})`;
     }
+
+    bgimage.style.transition = "background-image 1s ease-in-out";
 },2);
 }
+
 export default Bgchange;
